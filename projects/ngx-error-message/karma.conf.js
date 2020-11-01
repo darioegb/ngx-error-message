51a69557d2fbb07f2c23174c6f5ddf2d8732d255
Dario Gonzalez Bano <Dario.GonzalezBano@thomsonreuters.com>
2020-11-01 13:00:16 -0300

    add puppeteer for test error

--- projects/ngx-error-message/karma.conf.js
+++ projects/ngx-error-message/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -31,7 +32,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
     browserDisconnectTolerance: 3,
