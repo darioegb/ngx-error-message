0423a237d0a0decc29dfcbccca9556b69ed8824b
darioegb <darioegb@gmail.com>
2021-03-24 17:59:23 +0000

    Develop

--- projects/ngx-error-message-showcase/e2e/src/app.e2e-spec.ts
+++ projects/ngx-error-message-showcase/e2e/src/app.e2e-spec.ts
@@ -12,14 +12,6 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('ngx-error-message-showcase');
   });
-  //  it('blur input firstName', () => {
-  //   const firstName = component.nameControls.firstName;
-  //   firstName.setValue(null);
-  //   const inputEl = <HTMLInputElement>fixture.debugElement.nativeElement.querySelector('input[formcontrolname="firstName"]');
-  //   inputEl.dispatchEvent(new InputEvent('blur'));
-  //   fixture.detectChanges();
-  //   expect(inputEl.classList).toContain('error-container');
-  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
